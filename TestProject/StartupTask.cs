e4a5031bffe5abd9f69863367a1fdd5ee602f1e3
bcdpawel <pablo.bz63@gmail.com>
2016-03-03 11:54:03 +0100

    First implementation

--- TestProject/StartupTask.cs
+++ TestProject/StartupTask.cs
@@ -13,13 +13,15 @@ namespace TestProject
     {
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            // 
-            // TODO: Insert code to perform background work
-            //
-            // If you start any asynchronous methods here, prevent the task
-            // from closing prematurely by using BackgroundTaskDeferral as
-            // described in http://aka.ms/backgroundtaskdeferral
-            //
+            Init();
+
+            // TO DO: implement
+        }
+
+        private void Init()
+        {
+            // Space for init task
+            WorkerProcess wp = new WorkerProcess(1);
         }
     }
 }
---
+++ TestProject/WorkerProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class WorkerProcess
+    {
+        private int _noThreads;
+
+        public WorkerProcess(int noThreads)
+        {
+            _noThreads = noThreads;
+        }
+    }
+}
